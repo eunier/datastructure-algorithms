dccfb97491ce35085b8ba990feb04cc6645b5a3e
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-01-14 20:29:46 -0500

    Add recursion sum exmaple.

--- recursion/sum.py
+++ recursion/sum.py
@@ -7,5 +7,13 @@ def list_sum(num_list):
     return the_sum
 
 
+def list_sum_rec(num_list):
+    if len(num_list) == 1:
+        return num_list[0]
+    else:
+        return num_list[0] + list_sum(num_list[1:])
+
+
 input_list = [1, 2, 3, 4]
 print(list_sum(input_list))
+print(list_sum_rec(input_list))
